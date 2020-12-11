2ba40c39c473c4918016861c9641f3985fccb962
Svetoslav <s.hristev@yahoo.com>
2020-12-11 06:05:23 +0200

    Add files via upload

---
+++ Python/OOP/DECORATORS/2.Type check.py
@@ -0,0 +1,29 @@
+def type_check(value):
+    def decorator(func):
+        def wrapper(par):
+            if value == type(par):
+                return func(par)
+            else:
+                return "Bad Type"
+
+        return wrapper
+
+    return decorator
+
+
+@type_check(int)
+def times2(num):
+    return num * 2
+
+
+print(times2(2))
+print(times2('Not A Number'))
+
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
+
+
+print(first_letter('Hello World'))
+print(first_letter(['Not', 'A', 'String']))
